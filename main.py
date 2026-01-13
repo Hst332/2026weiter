356d1474c4de238cfb3af5cfd01a05b94f4429cb
Hst332 <hst332@gmail.com>
2026-01-13 16:31:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from forecast_assets import run_all
 from forecast_writer import write_daily_summary
 
 def main():
+    print("MAIN STARTED")
     results = run_all()
+    print("RESULTS:", results)
     write_daily_summary(results)
+    print("FILE WRITTEN")
+
 
 if __name__ == "__main__":
     main()
